60d4d189e45f4ee34748291995f3ac046c1d6630
Kabanchik Vitos <Kabanchik Vitos>
2017-03-09 22:40:55 +0200

    Lecture 2 homework

--- src/homework/ClockAngel.java
+++ src/homework/ClockAngel.java
@@ -3,11 +3,26 @@ package edu.source.it.lectures.lecture2.homework;
 public class ClockAngel {
     public static void main(String[] args) {
         int hours = 2;
-        int mins = 30;
+        int mins = 61;
         int result = 0;
 
         /*Your code here*/
+        if (hours >= 0 && hours <= 12 && mins >= 0 && mins <= 60){
+            int hoursDegree = (360 / 12);
+            int minsDegree = (360 / 60);
+            if (mins * minsDegree > hours * hoursDegree) {
+                result = mins * minsDegree - hours * hoursDegree;
+            } else {
+                result = hours * hoursDegree - mins * minsDegree;
+            }
+            if (result > 180) {
+                result = 360 - result;
+            }
+            System.out.println("Angle between hours and minute narrows is " + result);
+        } else {
+            System.out.println("Angle between hours and minute narrows is cannot be identified");
+        }
 
-        System.out.println("Angle between hours and minute narrows is " + result);
+
     }
 }
